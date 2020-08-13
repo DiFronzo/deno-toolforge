d23ba7d79d8c46cecff4f89f6a9e530cd3bac37c
Andreas Kilde Lien <andrklie@stud.ntnu.no>
2020-08-14 00:40:23 +0200

    build

---
+++ build/precache-manifest.35005a9cdc51ff1d3cd2ac5def27f3a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8abd36720f82874f9470b5ab8cd8e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495cd14e4d060d0ff7d3",
+    "url": "/static/css/main.3b8ad8cd.chunk.css"
+  },
+  {
+    "revision": "e5b575f2ea8b1df2bafe",
+    "url": "/static/js/2.0d12262f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d12262f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495cd14e4d060d0ff7d3",
+    "url": "/static/js/main.95a6764c.chunk.js"
+  },
+  {
+    "revision": "91ac1af196180ad7ff0c",
+    "url": "/static/js/runtime-main.a5715504.js"
+  },
+  {
+    "revision": "bd5995e6cbf337e3f09a03ab04731c6f",
+    "url": "/static/media/Wikimedia_Deno_Logo.bd5995e6.svg"
+  }
+]);
